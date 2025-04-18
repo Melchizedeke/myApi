57091d69047c189a06d2424420b040fb90af8073
Igoche John Eigege <140503808+Melchizedeke@users.noreply.github.com>
2025-04-18 11:34:10 +0100

    Modify the models.py and the settings.py respectively

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length=150)
+    age = models.IntegerField()
+    country = models.CharField(max_length=30)
+    programming_language = models.CharField(max_length=40)
+
+
+    def __str__(self):
+        return self.name
+
