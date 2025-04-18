3d9e1f85fae89847f4037bcde36605a87cab50bc
Igoche John Eigege <140503808+Melchizedeke@users.noreply.github.com>
2025-04-18 13:46:45 +0100

    Modify these files

--- api/views.py
+++ api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Person
+from .serializers import PersonSerializer
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view(['GET', 'POST'])
+def personList(request):
+    if request.method == 'GET':
+        persons = Person.objects.all()
+        serializer = PersonSerializer(persons, many=True)
+        return Response(serializer.data)
+    
+    if request.method == 'POST':
+        serializer = PersonSerializer(request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
